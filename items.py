f6bde13a58829676f196555a90af692d40b7501c
mafeihu <1109091542@qq.com>
2021-09-07 14:06:06 +0800

    抓取豆瓣电影top250

--- items.py
+++ items.py
@@ -7,11 +7,11 @@ import scrapy
 
 
 class DoubanItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    # title = scrapy.Field()
-    # src = scrapy.Field()
-    # img = scrapy.Field()
-    num = scrapy.Field()
-    play_date = scrapy.Field()
+
+    serial_number = scrapy.Field()
+    movie_name = scrapy.Field()
+    introduce = scrapy.Field()
+    star = scrapy.Field()
+    evaluate = scrapy.Field()
+    describe = scrapy.Field()
     pass
--- spiders/douban_spider.py
+++ spiders/douban_spider.py
@@ -1,23 +1,43 @@
 import scrapy
 from douban.items import DoubanItem
 class DoubanSpiderSpider(scrapy.Spider):
-    # 名称
     name = 'douban_spider'
-    # 允许访问域名
     allowed_domains = ['movie.douban.com']
-    # 入口域名
-    start_urls = ['https://movie.douban.com/cinema/later/taiyuan']
+    start_urls = ['https://movie.douban.com/top250']
 
+    #默认解析方法
     def parse(self, response):
-        movie_list = response.xpath("//div[@id='showing-soon']/div")
+        # 循环电影条目
+        movie_list = response.xpath("//div[@class='article']//ol[@class='grid_view']/li")
         for i_item in movie_list:
+            # item文件导进来
             douban_item = DoubanItem()
-            # douban_item['title'] = i_item.xpath(".//h3/text()").extract_first()
-            # douban_item['src'] = i_item.xpath(".//a/@href/text()").extract_first()
-            # douban_item['img'] = i_item.xpath(".//img/@src/text()").extract_first()
-            douban_item['num'] = i_item.xpath(".//ul/li[@class='dt last']/span/text()").extract_first()
-            douban_item['play_date'] = i_item.xpath('.//ul/li[1]/text()').extract_first()
+            # 写详细的xpath,进行数据的解析
+            douban_item['serial_number'] = i_item.xpath(".//div[@class='item']//em/text()").extract_first()
+            douban_item['movie_name'] = i_item.xpath("./div/div[2]/div[1]/a/span[1]/text()").extract_first()
+            content = i_item.xpath(".//div[@class='bd']/p[1]/text()").extract()
+            # 数据的处理
+            for i_content in content:
+                content_s = "".join(i_content.split())
+                douban_item['introduce'] = content_s
+            douban_item['star'] = i_item.xpath(".//div[@class='star']/span[@class='rating_num']/text()").extract_first()
+            douban_item['evaluate'] = i_item.xpath(".//div[@class='star']/span[4]/text()").extract_first()
+            douban_item['describe'] = i_item.xpath(".//div[@class='bd']/p[@class='quote']/span/text()").extract_first()
+            # 需要将数据yield到pipelines里面去
             yield douban_item
+        #解析下一页规则,取后一页的xpath
+        next_link = response.xpath("//span[@class='next']/link/@href").extract()
+        if next_link:
+            next_link = next_link[0]
+            yield scrapy.Request("https://movie.douban.com/top250"+next_link, callback=self.parse)
+
+
+
+
+
+
+
+
 
 
 
